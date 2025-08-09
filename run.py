9d804c6266bdd8eff7ae382121cb7072f80698c7
OmarGamill <89210801+OmarGamill@users.noreply.github.com>
2025-08-09 15:55:48 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from writeHere import days, Shifts, table, Path_to_dataset, save_path
 from Pipline import pipline
 
 
-output_dataset = pipline(Path_to_dataset, save_path,save_data=False)
+output_dataset = pipline(Path_to_dataset, save_path)
+
